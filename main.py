769ed5bb471da30ccc9d6c97685325ac3c9e276d
RKuhl <ryanakuhl@gmail.com>
2020-01-14 10:07:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from openpyxl import load_workbook
+from bs4 import BeautifulSoup
 import pyperclip, re, easygui
+from openpyxl import load_workbook
 
 class FirstTab:
     def __init__(self, row):
@@ -8,12 +9,9 @@ class FirstTab:
 
 wb = load_workbook(easygui.fileopenbox(msg='Select file to edit', title='Select'))
 sheet_ranges = wb['Sheet1']
-replacements = []
-for row in sheet_ranges.rows:
-    x = FirstTab(row)
-    replacements.append(x)
+replacements = [FirstTab(row) for row in sheet_ranges.rows]
 
 working_html = pyperclip.paste()
 for r in replacements:
     working_html = re.sub(r.current, r.replacement, working_html, flags=re.IGNORECASE)
-pyperclip.copy(working_html)
+pyperclip.copy(BeautifulSoup(working_html, 'html.parser').prettify())
